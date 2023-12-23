817665262158b820b9e67f2419dc97807694be57
Ferhat Kaplan <fkmll2121@gmail.com>
2023-12-23 22:31:36 +0300

    Update CSS and HTML files

---
+++ shortly-website/js/script.js
@@ -0,0 +1,30 @@
+const input = document.getElementById("link-input");
+const linkForm = document.getElementById("link-form");
+const errMsg = document.getElementById("err-msg");
+
+linkForm.addEventListener("submit", formSubmit);
+
+function formSubmit(e) {
+  e.preventDefault();
+
+  if (input.value === "") {
+    errMsg.innerHTML = "Please enter something";
+    input.classList.add("border-red");
+  } else if (!isValidUrl(input.value)) {
+    errMsg.innerHTML = "Please enter a valid URL";
+    input.classList.add("border-red");
+  } else {
+    errMsg.innerHTML = "";
+    input.classList.remove("border-red");
+    alert("Success");
+  }
+}
+
+function isValidUrl(url) {
+  try {
+    new URL(url);
+    return true;
+  } catch (error) {
+    return false;
+  }
+}
