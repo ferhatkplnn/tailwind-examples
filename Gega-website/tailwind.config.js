7043438d4815b6d1e739ae2d739c5b7cdf7c381f
Ferhat Kaplan <fkmll2121@gmail.com>
2024-02-16 21:09:26 +0300

    Create header

---
+++ Gega-website/tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}", "./*.html"],
+  theme: {
+    container: {
+      center: true,
+      screens: {
+        lg: "1140px",
+        xl: "1140px",
+        "2xl": "1140px",
+      },
+    },
+    extend: {
+      fontFamily: {
+        gemunu: ["Gemunu Libre", "sans-serif"],
+        openSans: ["Open Sans", "sans-serif"],
+      },
+
+      colors: {
+        "gega-red": "#BC1A45",
+        "gega-melon": "#FFD369",
+        "gega-grey": "#DDDDDD",
+        "gega-white": "#F7F7F7",
+      },
+    },
+  },
+  plugins: [],
+};
